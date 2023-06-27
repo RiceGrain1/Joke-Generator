c1dfcfd2caeeb39ddac0c82b6f1797ffab7dfc54
RiceGrain1 <102405076+RiceGrain1@users.noreply.github.com>
2023-06-26 19:36:01 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+   colors: {'background': '#fab22e' 
+  },extend: {},
+  },
+  plugins: [],
+}
